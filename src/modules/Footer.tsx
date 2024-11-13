f768765ad9c95f5d7a95ffbf3ca4565af1eb3e21
Alexander Lind <alexander.lind03@gmail.com>
2024-11-13 18:40:58 +0000

    corrected bad stuff

--- src/modules/Footer.tsx
+++ src/modules/Footer.tsx
@@ -2,9 +2,6 @@
 function Footer() {
     return (
         <footer className="py-4 text-center">
-          <p>
-            Hemsidan är just nu under rekonstruktion. Vänligen ha överseende med detta!
-          </p>
 
         </footer>
       );
