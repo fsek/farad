97f6c651473669228b929deb6a9397624e5a5db8
hugolindblom <98970228+KazakhCat@users.noreply.github.com>
2024-11-13 19:39:04 +0100

    first example of headers, footers and home page (#2)

--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -2,16 +2,16 @@ import logo from '../assets/FARAD-logga.png';
 
 function Header() {
     return (
-        <header className="flex justify-start items-center w-screen">
+        <header className="sticky top-0 z-20 shadow-md flex justify-start items-center w-screen bg-white dark:bg-black">
           <a href="/" className="flex items-center">
             <img src={logo} alt="Logo" className="h-20 w-auto m-4" />
           </a>
           <h1 className="text-2xl font-bold">FARAD 2025</h1>
 
           <nav className="flex space-x-6 ml-auto mr-10">
-            <a href="/Studenter" className="text-white text-lg font-bold hover:text-gray-300">För studenter</a>
-            <a href="/Foretag" className="text-white text-lg font-bold hover:text-gray-300">För företag</a>
-            <a href="/OmOss" className="text-white text-lg font-bold hover:text-gray-300">Om oss</a>
+            <a href="/Studenter" className="text-black dark:text-white text-lg font-bold hover:text-gray-300">För studenter</a>
+            <a href="/Foretag" className="text-black dark:text-white text-lg font-bold hover:text-gray-300">För företag</a>
+            <a href="/OmOss" className="text-black dark:text-white text-lg font-bold hover:text-gray-300">Om oss</a>
           </nav>
         </header>
         
