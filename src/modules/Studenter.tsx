f6b3a37e781fd75be411b132ac4070c7a085e2b7
Alexander Lind <alexander.lind03@gmail.com>
2024-11-13 17:17:02 +0000

    add to För studenter

--- src/modules/Studenter.tsx
+++ src/modules/Studenter.tsx
@@ -1,3 +1,4 @@
+import smiley from '../assets/smiley.png';
 
 function Studenter() {
     return (
@@ -5,7 +6,15 @@ function Studenter() {
       <h1 className="text-4xl font-bold text-forange">
         Studenter
       </h1>
-      Här kommer information för studenter att publiceras.
+      <p className="text-lg font-bold text-white dark:text-black">
+        Vill du jobba på FARAD? Klart du vill! Då anmäler du dig på denna länk: <a href="https://forms.gle/HxVdxwSJN6AF2z6Q9" className="text-blue dark:text-graytext font-bold hover:text-blue">länk till formulär</a>
+      </p>
+      <p className="font-bold text-white dark:text-black">
+        Mer info kommer
+      </p>
+      <a href="" className="flex items-center">
+        <img src={smiley} alt="Smiley" className="h-20 w-auto m-4" />
+      </a>
     </div>
   );
 }
