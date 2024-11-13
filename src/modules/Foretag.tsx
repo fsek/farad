7e26edf169034e7a826c2776db17b6121adb22cc
Alexander Lind <alexander.lind03@gmail.com>
2024-11-13 21:08:30 +0000

    add price plan to företag

--- src/modules/Foretag.tsx
+++ src/modules/Foretag.tsx
@@ -5,7 +5,12 @@ function Foretag() {
       <h1 className="text-4xl font-bold text-forange">
         Företag
       </h1>
-      Här kommer information för företag att publiceras.
+      <div className="mx-40 my-8 text-center text-lg font-bold text-white dark:text-black">
+        I detta dokument finns information om de olika paket ni som företag har att välja mellan <a href="../assets/SVENSKA Prisinformation FARAD 2025.pdf" download className="text-lg font-bold hover:text-gray-300">Prisinformation FARAD 2025</a>
+      </div>
+      <div className="mx-40 my-8 text-center text-lg font-bold text-white dark:text-black">
+        In this document you as a company may find all of the different price plans that we offer you to choose from <a href="../assets/ENGELSKA Price information FARAD 2025.pdf" download className="text-lg font-bold hover:text-gray-300">Price plans and information FARAD 2025</a>
+      </div>
     </div>
   );
 }
