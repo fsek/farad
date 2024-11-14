ce263ef2c0e34e26cd8c725816a4ffca1cf18f0a
Alexander Lind <86437324+Kurtthecat@users.noreply.github.com>
2024-11-14 10:54:39 +0100

    Added message and link to För studenter (#1)
    * add message about webpage reconstruction
    
    * add to För studenter
    
    * corrected bad stuff
    
    * add message about webpage reconstruction
    
    * commiting because it ses I should
    
    * add price plan to företag
    
    * fixed text on gray bg always black
    
    ---------
    
    Co-authored-by: Hugo Lindblom <hugo.e.lindblom@gmail.com>

--- src/modules/OmOss.tsx
+++ src/modules/OmOss.tsx
@@ -1,12 +1,17 @@
+import farad_2025 from '../assets/handsome_farad.jpg';
 
 function OmOss() {
     return (
       <div className="flex flex-col items-center justify-center w-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-forange">
-        Vi är projektgruppen!
-      </h1>
-      Mer info kommer snart.
-    </div>
+        <h1 className="text-4xl font-bold text-forange">
+          Vi är projektgruppen!
+
+        </h1>
+        <div className="lg:container">
+          <img src={farad_2025} alt="Very handsome photo of FARAD 2025" className="h-auto w-2/3 mx-auto m-4" />
+        </div>
+        <p className="text-black font-bold">Mer info kommer snart</p>
+      </div>
   );
 }
 
