67a1ca14f1d24693e52d8ab17c0c10b784349543
Hugo Lindblom <hugo.e.lindblom@gmail.com>
2024-11-28 20:27:57 +0100

    fix dates on main

--- src/langs/en/translation.tsx
+++ src/langs/en/translation.tsx
@@ -11,7 +11,7 @@ const enTranslation = {
     "welcome": "Welcome to",
     "f-s": "the F-guilds",
     "career fair": "career fair",
-    "dates": "28-30 January 2025",
+    "dates": "30 January 2025",
     "quick desc": "Farad is the F-guild at LTH's career fair, where companies get the opportunity to get in touch with students from the programs Engineering Physics, Engineering Mathematics and Engineering Nanoscience.",
     "signup": "Register your company by contacting",
 
