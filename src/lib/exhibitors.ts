210b6d7b8a98c1b8759965723b25f7102226ad55
Åke Amcoff <ake@amcoff.net>
2025-11-04 20:00:40 +0100

    sort exhibitors alphabetically

--- src/lib/exhibitors.ts
+++ src/lib/exhibitors.ts
@@ -9,22 +9,10 @@ export interface Exhibitor {
 
 export const exhibitors: Exhibitor[] = [
 	{
-		name: 'Ericsson',
-		about: m.ericsson_desc,
-		logo: '/exhibitors/ericsson.png',
-		slug: 'ericsson'
-	},
-	{
-		name: 'Lightbringer',
-		about: m.lightbringer_desc,
-		logo: '/exhibitors/lightbringer.png',
-		slug: 'lightbringer'
-	},
-	{
-		name: 'If',
-		about: m.if_desc,
-		logo: '/exhibitors/if.png',
-		slug: 'if'
+		name: 'Algorithma',
+		about: m.algorithma_desc,
+		logo: '/exhibitors/algorithma.png',
+		slug: 'algorithma'
 	},
 	{
 		name: 'Axis',
@@ -33,16 +21,46 @@ export const exhibitors: Exhibitor[] = [
 		slug: 'axis'
 	},
 	{
+		name: 'CellaVision',
+		about: () => '',
+		logo: '/exhibitors/cellavision.png',
+		slug: 'cellavision'
+	},
+	{
 		name: 'D-fine',
 		about: m.dfine_desc,
 		logo: '/exhibitors/dfine.png',
 		slug: 'dfine'
 	},
 	{
-		name: 'Sveriges Ingenjörer',
-		about: m.sveing_desc,
-		logo: '/exhibitors/sveing.png',
-		slug: 'sveriges-ingenjorer'
+		name: 'Electrolux',
+		about: () => '',
+		logo: '/exhibitors/electrolux.png',
+		slug: 'electrolux'
+	},
+	{
+		name: 'Ericsson',
+		about: m.ericsson_desc,
+		logo: '/exhibitors/ericsson.png',
+		slug: 'ericsson'
+	},
+	{
+		name: 'ESA',
+		about: () => '',
+		logo: '/exhibitors/esa.svg',
+		slug: 'esa'
+	},
+	{
+		name: 'FOI',
+		about: () => '',
+		logo: '/exhibitors/foi.svg',
+		slug: 'foi'
+	},
+	{
+		name: 'Hitachi Energy',
+		about: () => '',
+		logo: '/exhibitors/hitachi.svg',
+		slug: 'hitachi-energy'
 	},
 	{
 		name: 'IAESTE',
@@ -51,16 +69,28 @@ export const exhibitors: Exhibitor[] = [
 		slug: 'iaeste'
 	},
 	{
-		name: 'Algorithma',
-		about: m.algorithma_desc,
-		logo: '/exhibitors/algorithma.png',
-		slug: 'algorithma'
+		name: 'If',
+		about: m.if_desc,
+		logo: '/exhibitors/if.png',
+		slug: 'if'
 	},
 	{
-		name: 'Nanolund',
-		about: m.nanolund_desc,
-		logo: '/exhibitors/nanolund.png',
-		slug: 'nanolund'
+		name: 'Lightbringer',
+		about: m.lightbringer_desc,
+		logo: '/exhibitors/lightbringer.png',
+		slug: 'lightbringer'
+	},
+	{
+		name: 'LINC',
+		about: m.linc_desc,
+		logo: '/exhibitors/linc.png',
+		slug: 'linc'
+	},
+	{
+		name: 'Lund Formula Student',
+		about: () => '',
+		logo: '/exhibitors/fs.png',
+		slug: 'formula-student'
 	},
 	{
 		name: 'Lunds lasercentrum',
@@ -69,10 +99,16 @@ export const exhibitors: Exhibitor[] = [
 		slug: 'llc'
 	},
 	{
-		name: 'SVL',
-		about: m.svl_desc,
-		logo: '/exhibitors/svl.png',
-		slug: 'svl'
+		name: 'Nanolund',
+		about: m.nanolund_desc,
+		logo: '/exhibitors/nanolund.png',
+		slug: 'nanolund'
+	},
+	{
+		name: 'Netcompany',
+		about: m.netcompany_desc,
+		logo: '/exhibitors/netcompany.png',
+		slug: 'netcompany'
 	},
 	{
 		name: 'Reglerteknik',
@@ -87,57 +123,21 @@ export const exhibitors: Exhibitor[] = [
 		slug: 'sentianai'
 	},
 	{
-		name: 'Netcompany',
-		about: m.netcompany_desc,
-		logo: '/exhibitors/netcompany.png',
-		slug: 'netcompany'
+		name: 'SVL',
+		about: m.svl_desc,
+		logo: '/exhibitors/svl.png',
+		slug: 'svl'
 	},
 	{
-		name: 'LINC',
-		about: m.linc_desc,
-		logo: '/exhibitors/linc.png',
-		slug: 'linc'
-	},
-	{
-		name: 'FOI',
-		about: () => '',
-		logo: '/exhibitors/foi.svg',
-		slug: 'foi'
-	},
-	{
-		name: 'CellaVision',
-		about: () => '',
-		logo: '/exhibitors/cellavision.png',
-		slug: 'cellavision'
-	},
-	{
-		name: 'Electrolux',
-		about: () => '',
-		logo: '/exhibitors/electrolux.png',
-		slug: 'electrolux'
-	},
-	{
-		name: 'ESA',
-		about: () => '',
-		logo: '/exhibitors/esa.svg',
-		slug: 'esa'
-	},
-	{
-		name: 'Formula Student',
-		about: () => '',
-		logo: '/exhibitors/fs.png',
-		slug: 'formula-student'
+		name: 'Sveriges Ingenjörer',
+		about: m.sveing_desc,
+		logo: '/exhibitors/sveing.png',
+		slug: 'sveriges-ingenjorer'
 	},
 	{
 		name: 'Unionen',
 		about: () => '',
 		logo: '/exhibitors/unionen.png',
 		slug: 'unionen'
-	},
-	{
-		name: 'Hitachi Energy',
-		about: () => '',
-		logo: '/exhibitors/hitachi.svg',
-		slug: 'hitachi-energy'
 	}
 ];
